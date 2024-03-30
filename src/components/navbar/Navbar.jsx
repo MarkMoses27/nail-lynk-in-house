7633cbf8d11cfc99ca2272c866b698b7a00d9b20
MarkMoses27 <markmoses817@gmail.com>
2024-03-30 11:29:56 +0300

    added files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,30 @@
 
 import { Link } from 'react-router-dom';
-import LOGO from '../../assets/lynk.svg'
+import LYNK_LOGO from '../../assets/lynk.svg';
 import './Navbar.css'; 
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="container">
-        <Link to="/" className="logo">
-          <img src ={LOGO} alt="Logo" className="logo" />
-        </Link>
+    <nav className="container">
+      <div className='logo'>
+        <img src={LYNK_LOGO} alt="logo" />
+      </div>
         <ul className="nav-links">
           <li>
             <Link to="/">Home</Link>
           </li>
+          <li> <Link to="/about">About </Link></li>
           <li>
             <Link to="/services">Services</Link>
           </li>
           <li>
             <Link to="/gallery">Gallery</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
+          <li><Link to="/pricing">Pricing</Link></li>
           <li>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-      </div>
     </nav>
   );
 };
